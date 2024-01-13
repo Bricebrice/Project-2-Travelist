c8e7d7660a9158e553e1721844f5d8f1e5261cec
Brice Cabioch <cabioch.brice@gmail.com>
2024-01-13 13:22:55 +0100

    Login/logout, cookies and session completed

--- travelist/routes/auth.routes.js
+++ travelist/routes/auth.routes.js
@@ -35,7 +35,55 @@ router.post("/signup", (req, res, next) => {
     .catch((error) => next(error));
 });
 
+// GET route ==> to display the login form to users
+router.get("/login", (req, res) => res.render("auth/login"));
+
+// POST login route ==> to process form data
+router.post("/login", (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, email and password to login.",
+    });
+    return;
+  }
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        console.log("Email not registered. ");
+        res.render("auth/login", {
+          errorMessage: "User not found and/or incorrect password.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        // res.render("users/user-profile", { user });
+        req.session.currentUser = user;
+        res.redirect("/userProfile");
+      } else {
+        console.log("Incorrect password. ");
+        res.render("auth/login", {
+          errorMessage: "User not found and/or incorrect password.",
+        });
+      }
+    })
+    .catch((error) => next(error));
+});
+
 // GET route ==> to redirect to user profile
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+router.get("/userProfile", (req, res) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
+
+// POST route ==> to logout and kill the session
+router.post("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) next(err);
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
