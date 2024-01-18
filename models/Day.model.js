40aaf89c4dc736abdc71f4f0815e2f3c772cbb36
Omayma El Hadari <om.elhadari@gmail.com>
2024-01-18 22:37:55 +0100

    add day model

---
+++ models/Day.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const daySchema = new Schema(
+  {
+    title:String,
+    location:String,
+    description: String,
+    image:String,
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const Day = mongoose.model("Comment", commentSchema);
+
+module.exports = Day;
