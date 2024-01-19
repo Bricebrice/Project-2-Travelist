537817f338079a38526ffef6b0c2401d0d25fd7b
Omayma El Hadari <om.elhadari@gmail.com>
2024-01-19 23:42:11 +0100

    little changes in post-list view, creation of day-create form, first settings of days routes

---
+++ routes/day.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const Post = require('../models/Post.model');
+const Day = require('../models/Day.model')
+const fileUploader = require("../config/cloudinary.config");
+
+//
+
+module.exports = router;
